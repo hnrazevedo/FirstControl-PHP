47c49747177ad913c7555518830414aec9e4c39d
Henri Azevedo <hnr.azevedo@gmail.com>
2020-08-13 21:35:42 -0300

    clean commit

--- app/views/global/error.view.php
+++ app/views/global/error.view.php
@@ -1,9 +1,24 @@
 <html>
     <head>
         <title>{{system.appname}} - Error {{error.code}}</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="shortcut icon" href="{{system.uri}}/assets/img/favicon.ico" type="image/x-icon">
+
+        <script src="{{system.uri}}/assets/js/module/forms.mod.js" type="module"></script>
+
+        <link href="{{system.uri}}/assets/css/main.css" rel="stylesheet" type="text/css">
+        <link href="{{system.uri}}/assets/css/dialog.css" rel="stylesheet" type="text/css">
+        <link href="{{system.uri}}/assets/css/forms.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <p><b>{{error.code}}</b>.That’s an error.</p>
-        <p>{{error.message}}</p>
+        <main>
+            <dialog open="open" class="fixed">
+                <div>
+                    <p><b>{{error.code}}</b>.That’s an error.</p>
+                    <p>{{error.message}}</p>
+                </div>
+            </dialog>
+        </main>
+        
     </body>
 </html>
