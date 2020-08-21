c0c2b5cea64d2e9646da21b7e44c732c93f2c162
Henri Azevedo <hnrazevedo@gmail.com>
2020-08-21 16:04:56 -0300

    clean commit

--- app/views/global/dialog_confirm.tpl.php
+++ app/views/global/dialog_confirm.tpl.php
@@ -6,7 +6,7 @@
             <li>Você tem certeza que deseja realizar está operação?</li>
             <li class="li-btn">
                 <div class="list_buttons">
-                    <button id="cfm_cancel">Cancelar</button>
+                    <button id="cfm_cancel" class="cancel">Cancelar</button>
                     <button id="cfm_confirm">Confirmar</button>
                 </div>
             </li>
