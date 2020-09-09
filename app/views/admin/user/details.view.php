3e095b0593b413b7e8e30ce429d6a971e9d18e19
Henri Azevedo <hnr.azevedo@gmail.com>
2020-09-09 17:16:01 -0300

    add bootstrap

--- app/views/admin/user/details.view.php
+++ app/views/admin/user/details.view.php
@@ -13,6 +13,8 @@
         <link href="{{ $system.uri }}/assets/css/forms.css" rel="stylesheet" type="text/css">
         <link href="{{ $system.uri }}/assets/css/datatables.css" rel="stylesheet" type="text/css">
 
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    
     </head>
     <body>
         <?= $this->include('../../global/background') ?>
@@ -52,7 +54,7 @@
                                 </li>
                                 <li>
                                     <div class="buttons">
-                                        <button class="submit">Atualizar</button>
+                                        <button class="submit btn btn-primary">Atualizar</button>
                                     </div>
                                 </li>
                             </ul>   
@@ -64,5 +66,9 @@
         </main>
         <?= $this->include('../../global/dialog_loading') ?>
         <?= $this->include('../../global/dialog_message') ?>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+    
     </body>
 </html>
