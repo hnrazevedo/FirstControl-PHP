4a8d1c1e068445fe6945953424953cd93bb80c1b
Henri Azevedo <hnr.azevedo@gmail.com>
2020-10-16 14:35:23 -0300

    add content

--- app/controllers/helpers/Viewer.php
+++ app/controllers/helpers/Viewer.php
@@ -4,7 +4,7 @@ namespace App\Controller\Helper;
 
 trait Viewer
 {
-    public function view($data): void
+    protected function view(array $data): void
     {
         \HnrAzevedo\Viewer\Viewer::path(SYSTEM['basepath'].'app/views/')->render('index', array_merge($data, $_SESSION['view']['data']));
     }
